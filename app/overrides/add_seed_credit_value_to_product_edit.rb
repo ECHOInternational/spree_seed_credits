e8a2fcb15e2a1093959f55ba37615565f9e75493
Nathan Flood <npflood@users.noreply.github.com>
2021-08-19 17:35:16 -0400

    Update add_seed_credit_value_to_product_edit.rb

--- app/overrides/add_seed_credit_value_to_product_edit.rb
+++ app/overrides/add_seed_credit_value_to_product_edit.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/products/_form',
   name: 'add_seed_credit_value_to_product_edit',
-  insert_after: "erb[loud]:contains('text_field :price')",
+  insert_after: "div[data-hook=admin_product_form_price]",
   text: "
     <%= f.field_container :seed_credit_value, class: ['form-group'] do %>
       <%= f.label :seed_credit_value, raw(Spree.t(:seed_credit_value)) %>
