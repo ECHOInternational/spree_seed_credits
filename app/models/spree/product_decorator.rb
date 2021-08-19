102404de71aac4f23e780841c0419844ffed39ac
Nathan Flood <npflood@users.noreply.github.com>
2021-08-19 16:45:44 -0400

    Update product_decorator.rb

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,8 +1,10 @@
 module Spree
-  Product.class_eval do
-    delegate :seed_credit_value, :seed_credit_value=, to: :master
-    delegate :max_items_allowed_per_order, :max_items_allowed_per_order=, to: :master
-    delegate :max_items_allowed_using_seed_credits, :max_items_allowed_using_seed_credits=, to: :master
-    delegate :has_seed_credit_value?, :items_limited_per_order?, :has_seed_credit_limit?, to: :master
+  module ProductDecorator
+    Product.class_eval do
+      delegate :seed_credit_value, :seed_credit_value=, to: :master
+      delegate :max_items_allowed_per_order, :max_items_allowed_per_order=, to: :master
+      delegate :max_items_allowed_using_seed_credits, :max_items_allowed_using_seed_credits=, to: :master
+      delegate :has_seed_credit_value?, :items_limited_per_order?, :has_seed_credit_limit?, to: :master
+    end
   end
 end
