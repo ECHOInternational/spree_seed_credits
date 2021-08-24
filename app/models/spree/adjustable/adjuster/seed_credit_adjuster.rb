328d2a41ebb6e04b4e535c6942f53fe9f4c9a9b1
Nathan Flood <npflood@users.noreply.github.com>
2021-08-24 12:53:45 -0400

    Update seed_credit_adjuster.rb

--- app/models/spree/adjustable/adjuster/seed_credit_adjuster.rb
+++ app/models/spree/adjustable/adjuster/seed_credit_adjuster.rb
@@ -3,9 +3,8 @@ module Spree
     module Adjuster
       class SeedCreditAdjuster < Spree::Adjustable::Adjuster::Base
         def update
-          byebug
           @totals[:non_taxable_adjustment_total] += adjustments.where(source_type: "Spree::SeedCredit").reload.map(&:update!).compact.sum
-          unless @adjustable.class == Spree::Shipment
+          if @adjustable.class == Spree::LineItem
             @totals[:seed_credits_used] = adjustments.where(source_type: "Spree::SeedCredit").sum(&:credit_count)
           end
         end
